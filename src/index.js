98778bc8262e35bf8e7b63b2b94377eb3bfd6a58
chrymcs <chrymcs@gmail.com>
2022-10-09 12:57:10 +0300

    Mike's version

--- src/index.js
+++ src/index.js
@@ -3,60 +3,57 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGTH = canvas.height = 700;
 
-let gamespeed = 10;
-const Layer1 = new Image();
-const Layer2 = new Image();
-const Layer3 = new Image();
-const Layer4 = new Image();
-const Layer5 = new Image();
-Layer1.src = 'assets\\layer1.png';
-Layer2.src = 'assets\\layer2.png';
-Layer3.src = 'assets\\layer3.png';
-Layer4.src = 'assets\\layer4.png';
-Layer5.src = 'assets\\layer5.png';
+const GAMESPEED = 10;
+const ImageLayer1 = new Image();
+const ImageLayer2 = new Image();
+const ImageLayer3 = new Image();
+const ImageLayer4 = new Image();
+const ImageLayer5 = new Image();
+ImageLayer1.src = 'assets\\layer1.png';
+ImageLayer2.src = 'assets\\layer2.png';
+ImageLayer3.src = 'assets\\layer3.png';
+ImageLayer4.src = 'assets\\layer4.png';
+ImageLayer5.src = 'assets\\layer5.png';
+const layerConfig = {
+    x: 0,
+    y: 0,
+    width:2400,
+    height: 700,
+    x2:2400,
+    maxImageWidth:2400
+}
 
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.x2 = 2400;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gamespeed * this.speedModifier;
-    }
-    update() {
-        this.speed = gamespeed * this.speedModifier;
-        if (this.x < -2400) {
-            this.x = 2400 + this.x2 - this.speed;
-        }
-        if (this.x2 < -2400) {
-            this.x2 = 2400 + this.x - this.speed;
-        }
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
-    }
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+const drawImageLayer = ({x, x2, speedModifier, maxImageWidth, width, height, image, y}) => {
+    ctx.drawImage(image, x, y, width, height);
+    ctx.drawImage(image, x2, y, width, height);
+
+    return {
+        image,height,width,y,speedModifier, maxImageWidth,x,x2
     }
 }
 
-const layer1 = new Layer(Layer1, 0.2);
-const layer2 = new Layer(Layer2, 0.2);
-const layer3 = new Layer(Layer3, 0.2);
-const layer4 = new Layer(Layer4, 0.3);
-const layer5 = new Layer(Layer5, 0.5);
+const updateImageLayer = ({x, x2, speedModifier, maxImageWidth, width, height, image, y}) => {
+    const speed = GAMESPEED * speedModifier;
+    if (x < -maxImageWidth) {
+        x = maxImageWidth + x2 - speed;
+    }
+    if (x2 < -maxImageWidth) {
+        x2 = maxImageWidth + x - speed;
+    }
+    return {
+        image,height,width,y,speedModifier, maxImageWidth,
+        x:Math.floor(x - speed),
+        x2:Math.floor(x2 - speed)
+    }
+}
 
-const gameObjects = [layer1,layer2,layer3,layer4,layer5];
+const baseGameObjects = [{image:ImageLayer1,speedModifier:0.2}, {image:ImageLayer2,speedModifier:0.2}, {image:ImageLayer3,speedModifier:0.2}, {image:ImageLayer4,speedModifier:0.3}, {image:ImageLayer5,speedModifier:0.5}]
+let enchancedGameObjects = baseGameObjects.map(baseGameObject => ({...baseGameObject, ...layerConfig}))
 
-function animate() {
+function animate(enchancedGameObjects) {
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGTH)
-    gameObjects.forEach(object => {
-        object.update();
-        object.draw();
-    });
-    requestAnimationFrame(animate);
+
+    let newenchancedGameObject = enchancedGameObjects.map(enchancedGameObject => drawImageLayer(updateImageLayer(enchancedGameObject)));
+    requestAnimationFrame(() => animate(newenchancedGameObject));
 };
-animate();
+animate(enchancedGameObjects);
